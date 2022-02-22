e64767a1e1f6755aa089d448f46b99cfb5a7af15
hashtegui <guilherme.castro.souza1999@gmail.com>
2022-02-22 10:35:44 -0400

    /home/ti/Documentos/repositorio/python/coleta_app/venv/bin/python

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from kivy.app import App
-from view.LoginView import KV
+#from view.LoginView import KV
+from view.MainView import MainWindow
 
 class Main(App):
     def build(self):
-        return KV
+        return MainWindow()
 
 if __name__ == '__main__':
     Main().run()
